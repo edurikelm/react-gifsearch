e136ba6894fc0dc3ba805253b44a621963f4d52b
Eduardo Riquelme <riquelme.mayorga@gmail.com>
2020-06-25 12:03:15 -0400

    docs agregado

---
+++ docs/precache-manifest.c614c79fdc53fe96d8bd04e6d63b5e7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b39128b55f9d61ee1465e5f32ae4a70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "318d967601e63402f846",
+    "url": "/static/css/main.32eba7a1.chunk.css"
+  },
+  {
+    "revision": "51e34cff51b285543b30",
+    "url": "/static/js/2.04409896.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.04409896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318d967601e63402f846",
+    "url": "/static/js/main.013225a9.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
